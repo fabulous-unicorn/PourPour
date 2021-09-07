d95ae479e15dafba29504f96a691ccfca476972e
Unicorn <alesya.volosach@gmail.com>
2021-09-07 13:47:33 +0300

     Добавлен экран инфо и его открытие(без largetitle)

---
+++ PourPour/Src/Scene/Info/InfoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  InfoViewController.swift
+//  PourPour
+//
+//  Created by Alesya on 07.09.2021.
+//
+
+import Foundation
+import UIKit
+
+class InfoViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
