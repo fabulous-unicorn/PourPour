4969b86bcc3a4ae03ed48acf4d88ad321067a585
Unicorn <alesya.volosach@gmail.com>
2021-10-07 14:23:57 +0300

    Добавлено отображение свойств рецепта из модели

--- PourPour/Src/Scene/Recipe/RecipeController.swift
+++ PourPour/Src/Scene/Recipe/RecipeController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class RecipeController: UIViewController{
     
+    @IBOutlet weak var coffeePropertyView: RecipePropertyView!
+    @IBOutlet weak var watterPropertyView: RecipePropertyView!
+    @IBOutlet weak var temperaturePropertyView: RecipePropertyView!
+    @IBOutlet weak var timePropertyView: RecipePropertyView!
     @IBOutlet weak var descriptionView: UITextView!
     
     var presenter: RecipePresenter!
@@ -42,5 +46,21 @@ extension RecipeController: RecipeView {
         
         self.title = recipe.name
         self.descriptionView.text = recipe.description
+        
+        self.coffeePropertyView.valueLabel = NSNumber(value: recipe.massCoffee).stringValue
+        self.watterPropertyView.valueLabel = "\(recipe.massWatter)"
+        self.temperaturePropertyView.valueLabel = "\(recipe.temperature)"
+        self.timePropertyView.valueLabel = formMinutesTimerString(from: recipe.time)
+    }
+    
+    func formMinutesTimerString(from seconds: Int) -> String {
+        let minutesTimer = seconds / 60
+        let secondsTimer = seconds - minutesTimer * 60
+        
+        if minutesTimer<10 {
+            return "0\(minutesTimer):\(secondsTimer)"
+        } else {
+            return "\(minutesTimer):\(secondsTimer)"
+        }
     }
 }
