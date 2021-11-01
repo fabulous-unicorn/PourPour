7bcc66835924d2789fdc231082adce69ba7ff1e6
Unicorn <alesya.volosach@gmail.com>
2021-11-01 14:38:40 +0300

    ~небольшие изменения в логике

---
+++ PourPour/Src/Scene/RunningRecipe/Views/CommonTime.swift
@@ -0,0 +1,20 @@
+//
+//  CommonTime.swift
+//  PourPour
+//
+//  Created by Alesya on 01.11.2021.
+//
+
+import SwiftUI
+
+struct CommonTime: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CommonTime_Previews: PreviewProvider {
+    static var previews: some View {
+        CommonTime()
+    }
+}
---
+++ PourPour/Src/Scene/RunningRecipe/Views/RunningRecipeSubhead.swift
@@ -0,0 +1,20 @@
+//
+//  RunningRecipeSubhead.swift
+//  PourPour
+//
+//  Created by Alesya on 01.11.2021.
+//
+
+import SwiftUI
+
+struct RunningRecipeSubhead: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RunningRecipeSubhead_Previews: PreviewProvider {
+    static var previews: some View {
+        RunningRecipeSubhead()
+    }
+}
