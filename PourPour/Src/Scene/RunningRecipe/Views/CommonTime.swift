b53a73ea19fcf05d15b2a4d74d2f72d44e41451d
Unicorn <alesya.volosach@gmail.com>
2021-11-01 14:44:53 +0300

    добавленно принятие переменных в подзаголовок

--- PourPour/Src/Scene/RunningRecipe/Views/CommonTime.swift
+++ PourPour/Src/Scene/RunningRecipe/Views/CommonTime.swift
@@ -8,13 +8,33 @@
 import SwiftUI
 
 struct CommonTime: View {
+    var currentTime: Int
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Group {
+            HStack {
+                Text("Общее время")
+                    .font(.system(size: 20))
+                    .fontWeight(.medium)
+                    .foregroundColor(Color("text-secondary"))
+                    .frame(height:24)
+                Spacer()
+                Text(TimeFormaterr.formMinutesTimerString(from: currentTime))
+                    .font(.system(size: 22))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color("text-primary"))
+                    .frame(height: 28.0)
+                    .padding(/*@START_MENU_TOKEN@*/.vertical, 12.0/*@END_MENU_TOKEN@*/)
+            }
+            .padding(.horizontal, 16.0)
+            .background(Color("surface-secondary-bg"))
+        }
     }
 }
 
 struct CommonTime_Previews: PreviewProvider {
     static var previews: some View {
-        CommonTime()
+        CommonTime(currentTime: 13)
+            .previewLayout(.sizeThatFits)
     }
 }
--- PourPour/Src/Scene/RunningRecipe/Views/RunningRecipeSubhead.swift
+++ PourPour/Src/Scene/RunningRecipe/Views/RunningRecipeSubhead.swift
@@ -8,13 +8,24 @@
 import SwiftUI
 
 struct RunningRecipeSubhead: View {
+    let numberCurrentStep: Int
+    let massWatter: Int
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Шаг \(self.numberCurrentStep): Влейте \(self.massWatter) гр вода")
+            .font(.system(size: 22))
+            .fontWeight(.bold)
+            .foregroundColor(Color("text-primary"))
+            .tracking(0.8)
+            .multilineTextAlignment(.center)
+            .padding(.top, 24.0)
+            .padding(.bottom, 8.0)
     }
 }
 
 struct RunningRecipeSubhead_Previews: PreviewProvider {
     static var previews: some View {
-        RunningRecipeSubhead()
+        RunningRecipeSubhead(numberCurrentStep: 2, massWatter: 50)
+            .previewLayout(.sizeThatFits)
     }
 }
